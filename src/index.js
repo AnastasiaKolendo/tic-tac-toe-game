28333bfd5d21145190736329c5d28b079c8a723b
Anastasia Kolendo <kolendoanastasiya@gmail.com>
2021-09-17 21:05:57 -0700

    Add taking terms

--- src/index.js
+++ src/index.js
@@ -2,23 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
-class Square extends React.Component {
-    constructor(props) {
-        super(props);
+function Square(props) {
 
-        this.state = {
-            value: null,
-        }
-    }
 
-    render() {
-        return (
-            <button className="square"
-                onClick={() => this.setState({ value: 'X' })}>
-                {this.state.value}
-            </button>
-        );
-    }
+    return (
+        <button
+            className="square"
+            onClick={() => props.onClick()}>
+            {props.value}
+        </button>
+    );
+
 }
 
 class Board extends React.Component {
@@ -26,16 +20,23 @@ class Board extends React.Component {
         super(props);
         this.state = {
             squares: Array(9).fill(null),
+            xIsNext: true,
         };
     }
 
     renderSquare(i) {
         return <Square value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)} />;
+            onClick={() => this.handleClick(i)} />;
+    }
+
+    handleClick(i) {
+        const squares = this.state.squares.slice();
+        squares[i] = this.state.xIsNext ? 'X' : 'O';
+        this.setState({ squares: squares, xIsNext: !this.state.xIsNext });
     }
 
     render() {
-        const status = 'Next player: X';
+        const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
 
         return (
             <div>
